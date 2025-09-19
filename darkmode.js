3090cbbf6d9d9629d16444ba69b54bb906fca872
mywebsitenotyours <jri6mail@gmail.com>
2025-09-19 12:15:50 -0400

    added sunglass button

--- darkmode.js
+++ darkmode.js
@@ -3,21 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jacob Isaacs - Articles</title>
+    <title>Your Name - Articles</title>
     <link rel="stylesheet" href="style.css">
-    <script src="darkmode.js"></script>
 </head>
 <body>
     <header>
         <h1>Jacob Isaacs</h1>
-        <p class="add-sunglasses-btn">Feast your eyes upon my beautiful website. You might want to wear sunglasses so the beauty doesn't blind you. Poke around if it tickles your fancy.</p>
+        <p>Feast your eyes upon my beautiful website. You might want to wear sunglasses so the beauty doesn't blind you. Poke around if it tickles your fancy.</p>
+        
     </header>
     
     <main>
         <h2>Articles</h2>
         <ul>
             <li><a href="article1.html">We Need Digital Manners</a> - 09 September 2025</li>
-            <li><a href="article2.html">My Review of Breakneck by Dan Wang</a> - 14 September 2025</li>
+            <li><a href="article2.html">My Review of Breackneck by Dan Wang</a> - 14 September 2025</li>
             <li><a href="article3.html">What does art look like in a future with AI?</a> - 16 September 2025</li>
         </ul>
     </main>
